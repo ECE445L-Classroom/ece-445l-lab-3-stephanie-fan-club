a9c4a6405e90ee29acad0da6699bc1c06260b0df
stephaniegoff25 <stephaniegoff@utexas.edu>
2024-09-18 16:12:37 -0500

    button init yippee

--- sw/src/buttons.c
+++ sw/src/buttons.c
@@ -1,5 +1,14 @@
 #include "buttons.h"
 
+
 void switchInit(){
+	const uint32_t portf = 0x20;
+	const uint32_t pin0 = 0x1;
+	const uint32_t pin4 = 0x10;
 	
+	SYSCTL_RCGCGPIO_R |= portf; //activate portf
+	while(!(SYSCTL_RCGCGPIO_R&portf)){}; // sets clock then waits for it
+	
+	GPIO_PORTF_DIR_R &= ~(pin4|pin0);
+	GPIO_PORTF_DEN_R |= (pin4|pin0);		
 }
---
+++ sw/src/buttons.h
@@ -0,0 +1,12 @@
+#ifndef BUTTONS_H
+#define BUTTONS_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "../inc/ST7735.h"
+#include "../inc/PLL.h"
+#include "../inc/tm4c123gh6pm.h"
+
+
+
+#endif
